ced206659afe1a0330c6be6283581008d4a27a47
Daven Quinn <dev@davenquinn.com>
2015-10-13 21:45:09 -0700

    Updated finding of test fixtures

--- roi_read/test_roi_read.py
+++ roi_read/test_roi_read.py
@@ -7,12 +7,21 @@ image_sizes = dict(
     HRL0000B8C2=(320,480))
 
 
+def fixture(*args):
+    """
+    Gets a test fixture by path
+    """
+    fn = path.join(
+        path.dirname(__file__),
+        '..','test-fixtures',*args)
+
+
 def test_import_roi():
     """
     Tests that we can successfully import a ROI from
     an ENVI 5 XML file.
     """
-    fn = path.join(path.dirname(__file__),'..','test-fixtures','FRT0000CBE5.xml')
+    fn = fixture('FRT0000CBE5.xml')
     data = list(get_regions(fn))
     assert len(data) == 20
 
